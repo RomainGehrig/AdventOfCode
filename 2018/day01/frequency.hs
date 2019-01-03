97ab6e2a21ea05e37a42727dc37a81320107020d
Romain Gehrig <romain.gehrig@gmail.com>
2019-01-04 00:56:09 +0100

    Merge day 1 solutions into one file

--- 2018/day01/frequency.hs
+++ 2018/day01/frequency.hs
@@ -1,11 +1,23 @@
-import Data.Char (digitToInt, isDigit)
+import qualified Data.Set as S
 
 readInput :: IO [Int]
 readInput = do
   content <- readFile "input.txt"
   return $ map (read . filter (/= '+')) $ lines $ content
 
+repeated :: Ord a => [a] -> [a]
+repeated xss = go S.empty xss
+  where
+    go _ [] = []
+    go prev (x:xs) =
+      let xs' = go (x `S.insert` prev) xs in
+        if x `S.member` prev then
+          x : xs'
+        else
+          xs'
+
 main :: IO ()
 main = do
   digits <- readInput
   print $ sum $ digits
+  print $ head $ repeated $ scanl (+) 0 $ cycle digits
