4d21581f8b045ec47b8c001f9b27fe58e71747a3
Romain Gehrig <romain.gehrig@gmail.com>
2019-01-04 00:54:12 +0100

    Merge day 2 solutions into one file

--- 2018/day02/count.hs
+++ 2018/day02/count.hs
@@ -1,10 +1,10 @@
 import Data.List
 
+
 readInput :: IO [String]
 readInput = do
   content <- readFile "input.txt"
   return $ lines $ content
-
 
 count :: [String] -> (Int, Int)
 count xs = (length . filter twoLetters $ xs, length . filter threeLetters $ xs)
@@ -12,7 +12,14 @@ count xs = (length . filter twoLetters $ xs, length . filter threeLetters $ xs)
         threeLetters = counter 3
         counter n xs = any ((== n) . length . (`elemIndices` xs)) xs
 
+distance :: String -> String -> Int
+distance xs ys = length $ filter (\(x,y) -> x /= y) $ zip xs ys
+
+findUniqueDist1 :: [String] -> String
+findUniqueDist1 ids = map fst $ filter (\(x,y) -> x == y) $ uncurry zip $ head $ [ (x,y) | x <- ids, y <- ids, distance x y == 1 ]
+
 main :: IO ()
 main = do
   ids <- readInput
-  print $ (\(x,y) -> x*y) $ count $ ids
+  print $ (\(x,y) -> x*y) $ count ids
+  print $ findUniqueDist1 ids
